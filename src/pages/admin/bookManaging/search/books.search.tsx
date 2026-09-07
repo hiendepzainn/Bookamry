18ba28eb684d25a294ad6879ae239f6d2597a35d
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2026-09-07 21:20:31 +0700

    BOOK MANAGING: design SEARCH BAR

--- src/pages/admin/bookManaging/search/books.search.tsx
+++ src/pages/admin/bookManaging/search/books.search.tsx
@@ -1,5 +1,29 @@
+// import { Button, Input, Space } from "antd";
+
+import { Button, Input, Space } from "antd";
+
 const BookSearch = () => {
-  return <div>Book Search</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        marginBottom: "25px",
+      }}
+    >
+      <div style={{ width: "75%" }}>
+        <span style={{ marginRight: "10px" }}>Tên sách:</span>
+        <Input style={{ width: "40%", marginRight: "20px" }} />
+
+        <span style={{ marginRight: "10px" }}>Tác giả:</span>
+        <Input style={{ width: "40%" }} />
+      </div>
+      <Space>
+        <Button>Reset</Button>
+        <Button type="primary">Search</Button>
+      </Space>
+    </div>
+  );
 };
 
 export default BookSearch;
