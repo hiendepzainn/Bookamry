5c108401f69aa735802c7128a786dbbc73a06678
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2026-09-11 16:11:25 +0700

    BOOK MANAGING: customize + validate (type + size) IMAGES UPLOAD

--- src/pages/admin/bookManaging/table/tableComponents/createModal.tsx
+++ src/pages/admin/bookManaging/table/tableComponents/createModal.tsx
@@ -1,6 +1,7 @@
 import { getBookCategory } from "@/services/book.api";
 import { PlusOutlined } from "@ant-design/icons";
 import {
+  App,
   Col,
   Divider,
   Form,
@@ -12,6 +13,8 @@ import {
   Upload,
 } from "antd";
 import { useEffect, useState } from "react";
+import type { FormProps } from "antd";
+import { RcFile } from "antd/es/upload";
 
 interface IProps {
   isCreateModalOpen: boolean;
@@ -21,7 +24,13 @@ interface IProps {
 const BookCreateModal = (props: IProps) => {
   const { isCreateModalOpen, setIsCreateModalOpen } = props;
 
+  const { message } = App.useApp();
+
+  const [form] = Form.useForm();
+
   const [categoryList, setCategoryList] = useState<IBookCategory[]>([]);
+
+  const [isThumbnailUploaded, setIsThumbnailUploaded] = useState(false);
 
   const fetchCategory = async () => {
     const list: IBookCategory[] = [];
@@ -40,6 +49,27 @@ const BookCreateModal = (props: IProps) => {
     }
   };
 
+  const onFinish: FormProps<IBookCreate>["onFinish"] = (values) => {
+    console.log("Success:", values);
+  };
+
+  const beforeUploadThumbnail = (file: RcFile) => {
+    if (file.size > 2097152) {
+      message.error("File must < 2MB");
+      return Upload.LIST_IGNORE;
+    }
+    setIsThumbnailUploaded(true);
+    return false;
+  };
+
+  const beforeUploadSlider = (file: RcFile) => {
+    if (file.size > 2097152) {
+      message.error(`Invalid file size: ${file.name}`);
+      return Upload.LIST_IGNORE;
+    }
+    return false;
+  };
+
   useEffect(() => {
     fetchCategory();
   }, []);
@@ -51,14 +81,14 @@ const BookCreateModal = (props: IProps) => {
       open={isCreateModalOpen}
       okText="Tạo mới"
       cancelText="Hủy"
-      onOk={() => {}}
+      onOk={() => form.submit()}
       onCancel={() => {
         setIsCreateModalOpen(false);
       }}
     >
       <Divider />
 
-      <Form layout="vertical">
+      <Form onFinish={onFinish} form={form} layout="vertical">
         <Row gutter={16}>
           <Col span={12}>
             <Form.Item<IBookCreate> label="Tên sách" name="mainText" rules={[]}>
@@ -110,18 +140,34 @@ const BookCreateModal = (props: IProps) => {
               name="thumbnail"
               rules={[]}
             >
-              <Upload listType="picture-card">
-                <div>
-                  <PlusOutlined />
-                  <div>Upload</div>
-                </div>
+              <Upload
+                accept=".jpg,.JPG,.jpeg,.JPEG,.png,.PNG"
+                onRemove={() => setIsThumbnailUploaded(false)}
+                beforeUpload={beforeUploadThumbnail}
+                listType="picture-card"
+                showUploadList={{ showPreviewIcon: false }}
+              >
+                {isThumbnailUploaded ? (
+                  ""
+                ) : (
+                  <div>
+                    <PlusOutlined />
+                    <div>Upload</div>
+                  </div>
+                )}
               </Upload>
             </Form.Item>
           </Col>
 
           <Col span={12}>
             <Form.Item<IBookCreate> label="Ảnh Slider" name="slider" rules={[]}>
-              <Upload listType="picture-card">
+              <Upload
+                accept=".jpg,.JPG,.jpeg,.JPEG,.png,.PNG"
+                multiple={true}
+                beforeUpload={beforeUploadSlider}
+                listType="picture-card"
+                showUploadList={{ showPreviewIcon: false }}
+              >
                 <div>
                   <PlusOutlined />
                   <div>Upload</div>
